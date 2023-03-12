f62fe34f8e3f6fa7cfca555689a39ef7315bc4e7
Becca Morel <rebk@MacBook-Pro-de-Becca.local>
2023-03-11 22:20:34 -0500

    CALENDAAAAAAR

--- vue-client/src/views/CalendarView.vue
+++ vue-client/src/views/CalendarView.vue
@@ -10,9 +10,20 @@
             </div>
         </header>
         <section class="py-5">
-          <div class="container px-4 px-lg-5 mt-5">
-            heyyyyyy
+          <div class="container px-4 px-lg-5 mt-5 justify-content-between">
+            <div class="container border">
+                <vue-cal :disable-views="['years', 'year']" :time-from="8 * 60" :time-to="23 * 60" class="vuecal--blue-theme" style="height: 750px"/>
+            </div>
           </div>
         </section>
     </div>
 </template>
+<script>
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+export default {
+  components: {
+    VueCal
+  }
+}
+</script>
