0b1b66c69f3cbd47833c7f8278a8bd4703ac1aca
KycieK <victor.leonard41@gmail.com>
2023-03-18 15:52:15 -0400

    pageblanchegpeur

--- vue-client/src/main.js
+++ vue-client/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from '@vuex'
+import store from '@/vuex'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/css/styles.css'
